03af27a6971f47244de5bed923b54e74e3112069
samomi9ja <samuelomilade@gmail.com>
2023-06-15 19:05:44 +0100

    sidebar created

---
+++ Frontend/js/studentpay.js
@@ -0,0 +1,9 @@
+<script> 
+let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+
+btn.onclick = function() {
+    sidebar.classList.toggle('active');
+}
+
+</script>
